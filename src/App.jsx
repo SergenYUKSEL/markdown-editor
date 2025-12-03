6c6d9f38315ec8f4471ab3378642f7eb0015a346
Sergen <yukselsergen1@gmail.com>
2025-12-03 12:21:46 +0100

    Refactors routing in App component to use createBrowserRouter
    Replaces the previous routing setup with the modern createBrowserRouter approach from React Router v6.4+. This change simplifies the routing configuration and enhances the overall structure of the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
 import FilesView from './features/files/FilesView.jsx'
 import BlocksView from './features/blocks/BlocksView.jsx'
 import ImagesView from './features/images/ImagesView.jsx'
 import CollaborationsView from './features/collaborations/CollaborationsView.jsx'
 
+// Configuration des routes avec createBrowserRouter (approche moderne React Router v6.4+)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <FilesView />,
+      },
+      {
+        path: 'files',
+        element: <FilesView />,
+      },
+      {
+        path: 'blocks',
+        element: <BlocksView />,
+      },
+      {
+        path: 'images',
+        element: <ImagesView />,
+      },
+      {
+        path: 'collaborations',
+        element: <CollaborationsView />,
+      },
+    ],
+  },
+])
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<FilesView />} />
-          <Route path="files" element={<FilesView />} />
-          <Route path="blocks" element={<BlocksView />} />
-          <Route path="images" element={<ImagesView />} />
-          <Route path="collaborations" element={<CollaborationsView />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
